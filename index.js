6eec06726ed75cbf427566a1f4bdca862b64b7ae
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2022-03-24 00:39:20 +0100

    paystack integration

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+
+require("dotenv").config();//to use environmental variables
+
 // To connect with your mongoDB database
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 mongoose.connect(
-    'mongodb+srv://hotelbooking-db:droptop@cluster0.sw2l8.mongodb.net/hotelbooking-db?retryWrites=true&w=majority',
+    process.env.DB,
     {
         dbName: 'hotelbooking-db',
         useNewUrlParser: true,
@@ -26,6 +30,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    package: {
+        type: String,
+        required: true,
+    },
     date: {
         type: Date,
         default: Date.now,
@@ -38,8 +46,9 @@ RoomBooked.createIndexes();
 // For backend and express
 const express = require('express');
 const cors = require('cors');
-const sendmail = require('./mail');
-
+// const sendmail = require('./mail');
+const request = require('request');
+// const { initializePayment, verifyPayment } = require('./paystack')(request);
 
 const app = express();
 app.use(express.json());
@@ -50,7 +59,6 @@ app.use('/static', express.static('static'))//load static files
 
 
 const nodemailer = require('nodemailer');
-require("dotenv").config();//to use environmental variables
 
 let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -58,66 +66,82 @@ let transporter = nodemailer.createTransport({
     secure: false,
     requireTLS: true,
     auth: {
-       user: `${process.env.EMAIL_USERNAME}`,
-       pass: `${process.env.PASSWORD}`
+        user: `${process.env.EMAIL_USERNAME}`,
+        pass: `${process.env.PASSWORD}`
     }
 });
 
 app.get('/', (req, resp) => {
     resp.sendFile(__dirname + "/index.html");
 });
+app.get('/bookroom', (req, resp) => {
+    resp.sendFile(__dirname + "/bookroom.html");
+});
+app.get('/paynow', (req, resp) => {
+    resp.sendFile(__dirname + "/paynow.html");
+});
+
 //available rooms
-let availableRoom={
-    first_class:[1,2,3,4],
-    second_class:[5,6,7,8],
-    third_class:[9,10]
+let availableRoom = {
+    first_class: [1, 2, 3, 4],
+    second_class: [5, 6, 7, 8],
+    third_class: [9, 10]
 }
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 // Register data to book hotelroom
-app.post('/register', async (req, resp) => {
+app.post('/register', urlencodedParser, async (req, resp) => {
+    //try the follow if not okay catch handles it
     try {
         const user = new RoomBooked(req.body);
         let result = await user.save();
         result = result.toObject();
         if (result) {
             delete result.password;
-            resp.send(req.body);
-            console.log(result);
+            // resp.send(req.body);
 
+            // console.log(result);
+
+          
+            // resp.redirect('/pay');
             const mailConfigurations = {
 
                 // It should be a string of sender email
                 from: `${process.env.EMAIL_USERNAME}`,
-            
+    
                 // Comma Separated list of mails
-                to: 'oihimekpen@gmail.com',
-            
+                to: `${req.body.email}`,
+    
                 // Subject of Email
                 subject: `Room Booking Successful at Femson Hotel`,
-            
+    
                 // This would be the text of email body
                 text: `Your room no is ${req.body.roomNo}, `
                     + 'Lodge out time is 12 noon(12pm), '
-                    + `Enjoy your stay ${req.body.name}  `
+                    + `Enjoy your stay in our "${req.body.package}" ${req.body.name}  `
             };
-
+    
             transporter.sendMail(mailConfigurations, function (error, info) {
-                if (error) {throw Error(error);
-                }else{
-            
-               
-                console.log('Email Sent Successfully');
-                console.log(info);
-             }
+                if (error) {
+                    throw Error(error);
+                } else {
+    
+    
+                    console.log('Email Sent Successfully');
+    
+                    // console.log(info);
+                }
             });
-            
-            
-            
+
+
         } else {
             console.log('User already register');
         }
     } catch (err) {
-        resp.send('Something Went Wrong'+err);
+        resp.send('Something Went Wrong' + err);
     }
 });
 
@@ -129,6 +153,44 @@ app.get('/get-room-data', async (req, resp) => {
     } catch (error) {
         console.log(error);
     }
+});
+
+
+
+const MySecretKey = `"Bearer ${process.env.PAYSTACK_API_KEY}"`;
+
+
+
+app.get('/payment/verify', async (req, res) => {
+
+    const ref = req.query.reference;
+    let output;
+    await axios.get(`https://api.paystack.co/transaction/verify/${ref}`, {
+        headers: {
+            authorization: MySecretKey,
+            //replace TEST SECRET KEY with your actual test secret 
+            //key from paystack
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+        },
+    }).then((success) => {
+        output = success;
+    }).catch((error) => {
+        output = error;
+    });
+    //now we check for internet connectivity issues
+    if (!output.response && output.status !== 200) { throw  Error("No internet Connection"+error); }
+    //next,we confirm that there was no error in verification.
+    // if (output.response && !output.response.data.status) {
+    //     throw Error("Error verifying payment , 'unknown Transaction    Reference Id'");
+
+    // } 
+    else {
+        //we return the output of the transaction
+        res.status(200).send("Payment was successfully verified");
+        
+    }
+
 });
 
 
